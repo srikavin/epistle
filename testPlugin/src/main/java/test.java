1dc2b4eb925de37edc65e3d54475ffca29d26b91
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2016-11-12 21:20:24 -0500

    Updated test plugin to reflect updates

--- testPlugin/src/main/java/test.java
+++ testPlugin/src/main/java/test.java
@@ -5,34 +5,37 @@ import infuzion.chat.server.plugin.command.ICommandExecutor;
 import infuzion.chat.server.plugin.event.IEventListener;
 import infuzion.chat.server.plugin.event.chat.MessageEvent;
 import infuzion.chat.server.plugin.event.command.PreCommandEvent;
+import infuzion.chat.server.plugin.event.connection.JoinEvent;
 import infuzion.chat.server.plugin.event.reflection.EventHandler;
 
 public class test extends Plugin implements ICommandExecutor, IEventListener {
 
     @Override
     public void onEnable() {
-        System.out.println("312");
-        getCommandManager().registerCommand(new Command("abc"), this);
+        System.out.println("For testing: onEnable()");
+        getCommandManager().registerCommand(new Command("test"), this);
         getEventManager().registerEvents(this, this);
     }
 
     @Override
     public void onLoad() {
-        System.out.println("123");
+        System.out.println("For testing: onLoad()");
     }
 
     @Override
     public void onDisable() {
-        System.out.println("321");
+        System.out.println("For testing: onDisable()");
     }
 
 
     @Override
     public void onCommand(String commandName, String[] args, IChatClient client) {
-        client.sendMessage(commandName + " wowow");
+        client.sendMessage("Your inputted arguments are displayed below: ");
+        StringBuilder toSend = new StringBuilder();
         for (String e : args) {
-            client.sendMessage(e);
+            toSend.append(e).append("\n");
         }
+        client.sendMessage(toSend.toString());
     }
 
     @Override
@@ -42,13 +45,20 @@ public class test extends Plugin implements ICommandExecutor, IEventListener {
 
     @EventHandler
     public void onChat(MessageEvent event) {
-        event.setCanceled(true);
-        event.getSender().sendMessage("NOPE!");
+        if (event.getMessage().contains("cancel")) {
+            event.setCanceled(true);
+            event.getSender().sendMessage("Canceled!");
+        }
     }
 
     @EventHandler
     public void onCommand(PreCommandEvent event) {
+        event.setCanceled(false);
+        event.getSender().sendMessage("You issued the command " + event.getCommand());
+    }
+
+    @EventHandler
+    public void onJoin(JoinEvent event) {
         event.setCanceled(true);
-        event.getSender().sendMessage("NOPE!!!");
     }
 }
