c2eb55f983a68e67e226eefdee93b8d83e24563c
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2016-11-12 21:21:59 -0500

    Added copyright disclaimer

--- client/src/main/java/infuzion/chat/client/Client.java
+++ client/src/main/java/infuzion/chat/client/Client.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright DateInfo.year Infuzion
+ *  Copyright 2016 Infuzion
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
