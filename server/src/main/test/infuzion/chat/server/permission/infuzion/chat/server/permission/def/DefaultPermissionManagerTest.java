dc4708edde5d7a89ca15e3b877428143edde664c
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2016-11-22 18:10:16 -0500

    Update copyright

--- server/src/main/test/infuzion/chat/server/permission/infuzion/chat/server/permission/def/DefaultPermissionManagerTest.java
+++ server/src/main/test/infuzion/chat/server/permission/infuzion/chat/server/permission/def/DefaultPermissionManagerTest.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  *  Copyright 2016 Infuzion
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
+
 package infuzion.chat.server.permission.infuzion.chat.server.permission.def;
 
 import infuzion.chat.server.IChatClient;
@@ -12,9 +30,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by Sri on 11/22/2016.
- */
 public class DefaultPermissionManagerTest {
     @Test
     public void hasPermission() throws Exception {
